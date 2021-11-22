3930cfa8683fac1d267913d502452321c130ef35
ali <ali.gout.12@hotmail.com>
2021-11-22 10:48:06 +0100

    oefening 12

---
+++ robotarm-python-2021-main/example12.py
@@ -0,0 +1,21 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 12')
+
+# Jouw python instructies zet je vanaf hier:
+for blokjes in range(9):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color == "red":
+        for bewegenRechts in range(9):
+            robotArm.moveRight()
+        robotArm.drop()
+        for bewegenLinks in range(9 - blokjes):
+            robotArm.moveLeft()
+        robotArm.moveRight()
+    else:
+        robotArm.drop()
+        robotArm.moveRight()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
